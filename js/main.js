b2a797eaf8079594ef17a147dd80e431af18cc0c
AhmadHamdy1101 <32060651+AhmadHamdy1101@users.noreply.github.com>
2021-08-12 03:03:10 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
  let menu = document.querySelector(".menu");
  let openMenu = document.querySelector(".open-menu");
  let cloesMenu = document.querySelector(".close-meu");
+let login = document.querySelector(".login")
 
  openMenu.addEventListener('click', function(){
      menu.style.display = "flex";
      cloesMenu.style.display = "inline-block";
      openMenu.style.display = "none";
+     login.style.display = "none";
+
 
      
  });
@@ -13,5 +16,6 @@
     menu.style.display = "none";
     cloesMenu.style.display = "none";
     openMenu.style.display = "inline-block";
+   login.style.display = "block";
 
  })
